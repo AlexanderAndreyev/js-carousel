3fb9b684ec8acec18ecd2df6ed83befba07cecbf
Alexander <alexandr210993@gmail.com>
2017-04-15 22:29:00 +0300

    Bubbles were added

--- js/carousel.js
+++ js/carousel.js
@@ -5,12 +5,33 @@
       right = carousels[0].getElementsByClassName('right')[0],
       wrapper = carousels[0].getElementsByClassName('wrapper')[0],
       imgs = wrapper.getElementsByTagName('img'),
-      bubbles = carousels[0].getElementsByClassName('bubbles')[0],
+      bubblesContainer = carousels[0].getElementsByClassName('bubbles')[0],
       currentImgIndex = 0,
-      width = 500;
+      width = 500,
+      bubbles = [];
+
+  for (let i = 0; i < imgs.length; i++) {
+    let bubble = document.createElement('span');
+    bubble.classList.add('bubble');
+    bubblesContainer.appendChild(bubble);
+    bubbles.push(bubble);
+
+    bubble.addEventListener('click', function() {
+      currentImgIndex = i;
+      changeImg();
+    });
+  }
 
   function changeImg() {
     wrapper.style.left = -width * currentImgIndex + 'px';
+
+    bubbles.forEach(function(bubble, i) {
+      if (i === currentImgIndex) {
+        bubble.classList.add('active');
+      } else {
+        bubble.classList.remove('active');
+      }
+    });
   }
 
   right.addEventListener('click', function() {
@@ -34,4 +55,7 @@
     changeImg();
 
   });
+
+  changeImg();
+
 })();
